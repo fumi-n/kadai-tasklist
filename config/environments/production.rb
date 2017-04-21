7aa74cc1a238d6f2c00b69eb69549fddc9ea2cd1
yuis <ifgm238+git@gmail.com>
2017-04-21 13:44:17 +0000

    halt

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,6 +16,7 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   
   config.action_controller.perform_caching = true
+  
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
